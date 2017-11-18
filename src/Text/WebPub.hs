d40e5456f6df1d9d153720ce2a8ba2d7d21f7c60
Balletie <skip_meesie@hotmail.com>
2017-11-18 16:39:59 +0100

    Extract documents from EPUB using the spine and manifest

--- src/Text/WebPub.hs
+++ src/Text/WebPub.hs
@@ -1,20 +1,41 @@
+{-# LANGUAGE DisambiguateRecordFields, NamedFieldPuns #-}
 module Main
   (
     main
   )
   where
 
+import           Data.List (find)
+import           Data.Maybe (catMaybes)
 import qualified Data.ByteString.Lazy as B
 
+import           Control.Arrow ((&&&))
 import           Control.Monad.Except
 
 import           Codec.Archive.Zip
 import           Codec.Epub.Parse (getManifest, getSpine)
+import           Codec.Epub.Data.Manifest
+import           Codec.Epub.Data.Spine
 
 import           Text.WebPub.IO (getPkgPathXmlFromZip, getTocXmlFromZip)
 import           Text.WebPub.Parse (getNcxToc, reverseNcxToc, reverseHtmlToc)
 
-import           System.FilePath (takeDirectory)
+import           System.FilePath (takeFileName, takeDirectory, (</>))
+
+extractWithSpine :: (MonadIO m)
+                 => Spine            -- ^ The spine that specifies the order of files.
+                 -> Manifest         -- ^ The manifest to find the file locations.
+                 -> Archive          -- ^ The EPUB archive that stores the files.
+                 -> FilePath         -- ^ The directory of the container, to resolve paths.
+                 -> m [(FilePath, B.ByteString)] -- ^ The contents of all the documents.
+extractWithSpine spine (Manifest mis) archive relPath =
+  return $ map (eRelativePath &&& fromEntry) foundEntries
+  where itemRefs = map siIdRef $ spineItemrefs spine
+        maybePaths = map (\ref -> mfiHref <$> find ((ref ==) . mfiId) mis) itemRefs
+        maybeRelPaths = map (fmap (relPath </>)) maybePaths
+        maybeEntries = map (>>= (flip findEntryByPath $ archive)) maybeRelPaths
+        foundEntries = catMaybes maybeEntries
+
 
 main :: IO ()
 main = do
@@ -39,9 +60,9 @@ main = do
     tocXml' <- reverseNcxToc toc
     tocHtml <- reverseHtmlToc toc
 
+    inputDocuments <- extractWithSpine spine manifest zipArchive path
     liftIO $ putStrLn tocXml
     liftIO $ putStrLn $ show toc
-    liftIO $ putStrLn tocXml'
     liftIO $ putStrLn tocHtml
 
   either putStrLn return result
